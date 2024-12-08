5b21d76069c8dbd879896193b3f8ed3065084b6c
kavinda <kavindach4@gmail.com>
2024-12-08 14:53:38 +0530

    learn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-//import logo from './logo.svg';
-import './App.css';
-import x from './x.jpg'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={x} className="App-logo" alt="logo" />
-        <p>
-          I am Kavinda Supun
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is my Portfolio web
-        </a>
-      </header>
+  return(
+    <div className='container'>
+
     </div>
   );
 }
